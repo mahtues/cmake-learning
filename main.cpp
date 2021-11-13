441d991d693171a166104decd963aa8cb99e5caa
mahtues <14203456+mahtues@users.noreply.github.com>
2021-11-13 12:55:22 -0300

    external libs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Main.hpp>
 
 #include <foo.h>
 
 int main(int argc, char** argv) {
     std::cout << "hello world!\n";
     Foo().run();
+    sf::Int64 bar = 5;
+    std::cout << bar << "\n";
     return 0;
 }

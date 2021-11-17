af6c2999b37f82c79e50cc8493e4cd5bc7db9a2b
mahtues <14203456+mahtues@users.noreply.github.com>
2021-11-17 15:59:46 -0300

    opening window

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-#include <SFML/System.hpp>
-#include <SFML/Main.hpp>
+#include <GL/glut.h>
 
 #include <foo.h>
 
 int main(int argc, char** argv) {
     std::cout << "hello world!\n";
     Foo().run();
-    sf::Int64 bar = 5;
-    std::cout << bar << "\n";
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(800,450);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("hello window");
+
+    glutMainLoop();
+
     return 0;
 }
